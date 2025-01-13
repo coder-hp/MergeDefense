76847745ec23d33aa6d086bdb90fa896eef69c28
coder-hp <582254172@qq.com>
2025-01-13 18:37:08 +0800

    11

---
+++ MergeDefense/Assets/Resources/Toast/ToastScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using System;
+using DG.Tweening;
+
+public class ToastScript : MonoBehaviour {
+    public static void show (string text)
+    {
+        try
+        {
+            //GameObject prefab = Resources.Load("Toast/Toast") as GameObject;
+            //GameObject obj = Instantiate(prefab, LaunchScript.s_instance.canvas_top);
+            //obj.transform.Find("Text").GetComponent<Text>().text = text;
+
+            //obj.transform.DOLocalMove(new Vector3(0, 200, 0), 1).OnComplete(() =>
+            //{
+            //    Destroy(obj);
+            //});
+        }
+        catch(Exception exp)
+        {
+            Debug.Log("ToastScript.show异常：" + exp.ToString());
+        }
+    }
+}
