77a309d459526c107e3307a68c499bd734477934
coder-hp <582254172@qq.com>
2025-01-15 14:52:10 +0800

    11

--- MergeDefense/Assets/Scripts/Layers/GameLayer.cs
+++ MergeDefense/Assets/Scripts/Layers/GameLayer.cs
@@ -4,8 +4,23 @@ using UnityEngine;
 
 public class GameLayer : MonoBehaviour
 {
+    public List<Transform> enemyMoveFourPoint = new List<Transform>();
+    public Transform enemyPoint;
+
+    Vector3 enemyBornPos;
+
     void Start()
     {
+        enemyBornPos = enemyMoveFourPoint[0].position;
+
         LayerManager.ShowLayer(Consts.Layer.GameUILayer);
+
+        InvokeRepeating("onInvokeAddEnemy",1,1);
+    }
+
+    void onInvokeAddEnemy()
+    {
+        Transform enemyTrans = Instantiate(ObjectPool.getPrefab("Prefabs/Enemys/enemy1"), enemyPoint).transform;
+        enemyTrans.position = enemyBornPos;
     }
 }
