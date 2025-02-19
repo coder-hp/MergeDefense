56272ed5828a38df6002be9501d3e8172dd80eb3
coder-hp <582254172@qq.com>
2025-02-19 14:40:08 +0800

    11

--- MergeDefense/Assets/Scripts/Hero/HeroLogic102.cs
+++ MergeDefense/Assets/Scripts/Hero/HeroLogic102.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 // 射手
 // 单体攻击
-// 技能：攻击时，15%概率射出2支箭，持续3s
+// 技能：攻击时，25%概率射出2支箭
 public class HeroLogic102 : HeroBase
 {
     public override void AttackLogic(EnemyLogic enemyLogic)
@@ -12,5 +12,15 @@ public class HeroLogic102 : HeroBase
         Transform arrow = Instantiate(ObjectPool.getPrefab("Prefabs/Games/arrow"), GameLayer.s_instance.flyPoint).transform;
         arrow.position = heroLogicBase.centerPoint.position;
         arrow.GetComponent<ArrowLogic>().init(heroLogicBase.getAtk(), enemyLogic);
+
+        if(RandomUtil.getRandom(1,100) <= 25)
+        {
+            Transform arrow2 = Instantiate(ObjectPool.getPrefab("Prefabs/Games/arrow"), GameLayer.s_instance.flyPoint).transform;
+            arrow2.position = heroLogicBase.centerPoint.position;
+            arrow2.GetComponent<ArrowLogic>().init(heroLogicBase.getAtk(), enemyLogic);
+
+            // 把第一支箭往前挪一挪
+            arrow.position = Vector3.MoveTowards(arrow.position, enemyLogic.transform.position, 1);
+        }
     }
 }
