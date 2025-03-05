2dd249db059a286034efe6119fae6ca270f13367
coder-hp <582254172@qq.com>
2025-03-05 11:37:17 +0800

    11

---
+++ MergeDefense/Assets/Scripts/Layers/BossComingLayer.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BossComingLayer : MonoBehaviour
+{
+    public Text text_wave;
+
+    void Start()
+    {
+        text_wave.text = "WAVE " + (GameUILayer.s_instance.curBoCi + 1);
+
+        Destroy(gameObject,2);
+    }
+}
