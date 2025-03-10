372e18cd7cb587737153d810a744f4b82704c158
coder-hp <582254172@qq.com>
2025-03-10 13:11:57 +0800

    11

--- MergeDefense/Assets/Scripts/Layers/BossComingLayer.cs
+++ MergeDefense/Assets/Scripts/Layers/BossComingLayer.cs
@@ -5,11 +5,14 @@ using UnityEngine.UI;
 
 public class BossComingLayer : MonoBehaviour
 {
+    public Image img_icon;
     public Text text_wave;
 
     void Start()
     {
         AudioScript.s_instance.playSound("bossComing");
+
+        img_icon.sprite = AtlasUtil.getAtlas_game().GetSprite(EnemyWaveEntity.getInstance().getData(GameUILayer.s_instance.curBoCi + 1).prefab);
         text_wave.text = "WAVE " + (GameUILayer.s_instance.curBoCi + 1);
 
         Destroy(gameObject,2);
