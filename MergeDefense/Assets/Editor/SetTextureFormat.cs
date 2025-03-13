dc61b0ad1dad36277cdc3d21538bd073a2952e2e
coder-hp <582254172@qq.com>
2025-03-13 11:24:23 +0800

    11

--- MergeDefense/Assets/Editor/SetTextureFormat.cs
+++ MergeDefense/Assets/Editor/SetTextureFormat.cs
@@ -1,3 +1,4 @@
+using System.IO;
 using UnityEditor;
 using UnityEngine;
 
@@ -6,47 +7,62 @@ public class SetTextureFormat : MonoBehaviour
     [MenuItem("Assets/转换图片格式")]
     public static void SetFormat()
     {
-        // 获取选中的对象(文件夹)
+        // 获取选中的对象(文件夹或图片文件)
         // 可以同时选中多个文件夹，也可以点击文件夹内空白处获取当前所在文件夹
         Object[] selectedObjects = Selection.objects;
 
         foreach (Object selectedObject in selectedObjects)
         {
-            string folderPath = AssetDatabase.GetAssetPath(selectedObject);
-            //Debug.Log(folderPath);
+            string path = AssetDatabase.GetAssetPath(selectedObject);
 
-            // 获取文件夹下所有资源的 GUID
-            string[] guids = AssetDatabase.FindAssets("", new[] { folderPath });
-
-            // 遍历 GUID 并获取资源路径
-            foreach (string guid in guids)
+            // 如果选中的是文件夹
+            if (Directory.Exists(path))
             {
-                string assetPath = AssetDatabase.GUIDToAssetPath(guid);
-                //Debug.Log("Asset Path: " + assetPath);
+                // 获取文件夹下所有资源的 GUID
+                string[] guids = AssetDatabase.FindAssets("", new[] { path });
 
-                // 获取 TextureImporter
-                TextureImporter textureImporter = AssetImporter.GetAtPath(assetPath) as TextureImporter;
-
-                if (textureImporter != null)
+                // 遍历 GUID 并获取资源路径
+                foreach (string guid in guids)
                 {
-                    // 设置图片格式
-                    textureImporter.textureType = TextureImporterType.Sprite;
-                    // 设置压缩格式
-                    // textureImporter.textureCompression = TextureImporterCompression.Uncompressed;
-
-                    // 设置平台特定的格式
-                    //TextureImporterPlatformSettings settings = new TextureImporterPlatformSettings();
-                    //settings.format = TextureImporterFormat.RGBA32;                         // 设置格式为 RGBA32
-                    //settings.textureCompression = TextureImporterCompression.Compressed;    // 设置压缩
-                    //settings.compressionQuality = 50;                                       // 设置压缩质量
-                    //settings.maxTextureSize = 1024;                                         // 设置最大纹理尺寸
-
-                    //textureImporter.SetPlatformTextureSettings(settings);
-
-                    // 应用修改
-                    AssetDatabase.ImportAsset(assetPath, ImportAssetOptions.ForceUpdate);
+                    string assetPath = AssetDatabase.GUIDToAssetPath(guid);
+                    setFormat(assetPath);
                 }
             }
+            // 如果选中的是文件
+            else
+            {
+                setFormat(path);
+            }
+        }
+
+        Debug.Log("图片格式转换完成！");
+    }
+
+    static void setFormat(string assetPath)
+    {
+        // 获取 TextureImporter
+        TextureImporter textureImporter = AssetImporter.GetAtPath(assetPath) as TextureImporter;
+
+        if (textureImporter != null && textureImporter.textureType != TextureImporterType.Sprite)
+        {
+            Debug.Log("setFormat:" + assetPath);
+
+            // 设置图片格式
+            textureImporter.textureType = TextureImporterType.Sprite;
+            // 设置压缩格式
+            // textureImporter.textureCompression = TextureImporterCompression.Uncompressed;
+
+            // 设置平台特定的格式
+            //TextureImporterPlatformSettings settings = new TextureImporterPlatformSettings();
+            //settings.format = TextureImporterFormat.RGBA32;                         // 设置格式为 RGBA32
+            //settings.textureCompression = TextureImporterCompression.Compressed;    // 设置压缩
+            //settings.compressionQuality = 50;                                       // 设置压缩质量
+            //settings.maxTextureSize = 1024;                                         // 设置最大纹理尺寸
+
+            //textureImporter.SetPlatformTextureSettings(settings);
+
+            // 应用修改
+            AssetDatabase.ImportAsset(assetPath, ImportAssetOptions.ForceUpdate);
         }
     }
 }
