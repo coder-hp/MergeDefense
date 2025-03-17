03adc9078a7fedc7d5d76a5181bbd584843fae72
coder-hp <582254172@qq.com>
2025-03-17 17:38:01 +0800

    11

--- MergeDefense/Assets/Scripts/Hero/HeroLogic111.cs
+++ MergeDefense/Assets/Scripts/Hero/HeroLogic111.cs
@@ -9,20 +9,10 @@ using UnityEngine;
 // 技能3：攻击时，15%概率对范围内的敌人造成攻击力1000%的伤害
 public class HeroLogic111 : HeroBase
 {
-    int baseSkillRate = 25;
     public override void AttackLogic(EnemyLogic enemyLogic)
     {
         AudioScript.s_instance.playSound("111_attack");
-        Transform arrow = Instantiate(ObjectPool.getPrefab("Prefabs/Games/heroFlyWeapon102"), GameLayer.s_instance.flyPoint).transform;
-        arrow.GetComponent<heroFlyWeapon102>().init(heroLogicBase, enemyLogic);
-
-        if(RandomUtil.getRandom(1,100) <= (baseSkillRate + heroLogicBase.getAddSkillRate()))
-        {
-            Transform arrow2 = Instantiate(ObjectPool.getPrefab("Prefabs/Games/heroFlyWeapon102"), GameLayer.s_instance.flyPoint).transform;
-            arrow2.GetComponent<heroFlyWeapon102>().init(heroLogicBase, enemyLogic);
-
-            // 把第一支箭往前挪一挪
-            arrow.position = Vector3.MoveTowards(arrow.position, enemyLogic.transform.position, 1);
-        }
+        Transform arrow = Instantiate(ObjectPool.getPrefab("Prefabs/Games/heroFlyWeapon111"), GameLayer.s_instance.flyPoint).transform;
+        arrow.GetComponent<heroFlyWeapon111>().init(heroLogicBase, enemyLogic);
     }
 }
