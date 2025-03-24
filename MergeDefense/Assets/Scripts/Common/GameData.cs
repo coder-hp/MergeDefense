75e7413d89c6a1e81ca23c5451ef0a04f7ca862c
coder-hp <582254172@qq.com>
2025-03-24 13:33:22 +0800

    11

--- MergeDefense/Assets/Scripts/Common/GameData.cs
+++ MergeDefense/Assets/Scripts/Common/GameData.cs
@@ -76,6 +76,28 @@ public class GameData
         }
     }
 
+    public static int getPlayerExp()
+    {
+        return PlayerPrefs.GetInt("PlayerExp", 0);
+    }
+
+    public static void changePlayerExp(int value)
+    {
+        if (value == 0)
+        {
+            return;
+        }
+
+        int curCount = getPlayerExp() + value;
+        curCount = curCount < 0 ? 0 : curCount;
+        PlayerPrefs.SetInt("PlayerExp", curCount);
+
+        if (MainLayer.s_instance)
+        {
+            MainLayer.s_instance.refreshUI();
+        }
+    }
+
     public static int getIsOpenVibrate()
     {
         return PlayerPrefs.GetInt("IsOpenVibrate", 1);
