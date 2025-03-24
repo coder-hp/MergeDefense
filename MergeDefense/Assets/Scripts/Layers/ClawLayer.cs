14c6ae1fd40d3b897e4d06eb628693009296922b
MiyuyuInoue <104135844+MiyuyuInoue@users.noreply.github.com>
2025-03-24 18:04:30 +0800

    11111

--- MergeDefense/Assets/Scripts/Layers/ClawLayer.cs
+++ MergeDefense/Assets/Scripts/Layers/ClawLayer.cs
@@ -45,8 +45,8 @@ public class ClawLayer : MonoBehaviour
         AudioScript.s_instance.playSound_btn();
         clawTrans.DOLocalMoveY(-600, 3).SetEase(Ease.Linear).OnComplete(()=>
         {
-            clawTrans.Find("left").GetChild(0).DOLocalRotateQuaternion(Quaternion.Euler(0,0, 100),0.5f).SetDelay(1);
-            clawTrans.Find("right").GetChild(0).DOLocalRotateQuaternion(Quaternion.Euler(0, 0, -100), 0.5f).SetDelay(1).OnComplete(()=>
+            clawTrans.Find("left").GetChild(0).DOLocalRotateQuaternion(Quaternion.Euler(0,0, 105),0.5f).SetDelay(1);
+            clawTrans.Find("right").GetChild(0).DOLocalRotateQuaternion(Quaternion.Euler(0, 0, -105), 0.5f).SetDelay(1).OnComplete(()=>
             {
                 clawTrans.DOLocalMoveY(860, 3).SetDelay(1).SetEase(Ease.Linear);
             });
