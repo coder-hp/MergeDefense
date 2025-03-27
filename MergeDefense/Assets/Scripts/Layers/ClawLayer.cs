7104287b7341262fa3d81cf6cb7e659a08b3bd93
coder-hp <582254172@qq.com>
2025-03-27 17:37:34 +0800

    11

--- MergeDefense/Assets/Scripts/Layers/ClawLayer.cs
+++ MergeDefense/Assets/Scripts/Layers/ClawLayer.cs
@@ -10,7 +10,6 @@ public class ClawLayer : MonoBehaviour
     public Transform btn_claw;
     public Transform ballPointTrans;
 
-    float moveSpeed = 2;
     Vector3 startPos;
     Sequence seq_claw;
 
@@ -30,44 +29,53 @@ public class ClawLayer : MonoBehaviour
         AudioScript.s_instance.playMusic("bgm_claw", true);
     }
 
-    public void clawMove(bool isLeft)
-    {
-        if(isLeft)
-        {
-            clawTrans.Translate(-new Vector3(moveSpeed * Time.deltaTime,0,0));
-        }
-        else
-        {
-            clawTrans.Translate(new Vector3(moveSpeed * Time.deltaTime, 0, 0));
-        }
-    }
-
     int addedBallCount = 0;
     void onInvokeAddBall()
     {
-        Transform ballTrans =  Instantiate(prefab_ball, ballPointTrans).transform;
-        ballTrans.localPosition = new Vector3(RandomUtil.getRandom(-400,400),450,0);
-
-        if(++addedBallCount > 50)
+        for(int i = 0; i < 3; i++)
         {
-            btn_claw.localScale = Vector3.one;
-            CancelInvoke("onInvokeAddBall");
-
-            seq_claw = DOTween.Sequence();
-            seq_claw.Append(clawTrans.DOLocalMoveX(250,1).SetEase(Ease.Linear))
-               .Append(clawTrans.DOLocalMoveX(-250, 2).SetEase(Ease.Linear))
-               .Append(clawTrans.DOLocalMoveX(0, 1).SetEase(Ease.Linear)).SetLoops(-1);
+            Transform ballTrans = Instantiate(prefab_ball, ballPointTrans).transform;
+            if (RandomUtil.getRandom(1, 100) <= 50)
+            {
+                ballTrans.localPosition = new Vector3(-330 + RandomUtil.getRandom(-60, 60), 750 + RandomUtil.getRandom(0, 50), 0);
+            }
+            else
+            {
+                ballTrans.localPosition = new Vector3(330 + RandomUtil.getRandom(-60, 60), 750 + RandomUtil.getRandom(0, 50), 0);
+            }
+            ballTrans.GetComponent<Rigidbody2D>().velocity = new Vector2(0, -10);
         }
+
+        addedBallCount += 3;
+
+        if (addedBallCount > 33)
+        {
+            CancelInvoke("onInvokeAddBall");
+            Invoke("readyClaw",1);
+        }
+    }
+
+    void readyClaw()
+    {
+        btn_claw.localScale = Vector3.one;
+        seq_claw = DOTween.Sequence();
+        seq_claw.Append(clawTrans.DOLocalMoveX(158, 1).SetEase(Ease.Linear))
+           .Append(clawTrans.DOLocalMoveX(-158, 2).SetEase(Ease.Linear))
+           .Append(clawTrans.DOLocalMoveX(0, 1).SetEase(Ease.Linear)).SetLoops(-1);
     }
 
     public void onClickClaw()
     {
         AudioScript.s_instance.playSound_btn();
         seq_claw.Kill();
-        clawTrans.DOLocalMoveY(-250, 2).SetEase(Ease.Linear).OnComplete(()=>
+
+        clawTrans.Find("left/downGanZi").DOLocalRotateQuaternion(Quaternion.Euler(0, 0, -100), 0.5f);
+        clawTrans.Find("right/downGanZi").DOLocalRotateQuaternion(Quaternion.Euler(0, 0, -100), 0.5f);
+
+        clawTrans.DOLocalMoveY(-230, 2).SetEase(Ease.Linear).OnComplete(()=>
         {
-            clawTrans.Find("left").GetChild(0).DOLocalRotateQuaternion(Quaternion.Euler(0,0, 100),0.5f).SetDelay(1);
-            clawTrans.Find("right").GetChild(0).DOLocalRotateQuaternion(Quaternion.Euler(0, 0, -100), 0.5f).SetDelay(1).OnComplete(()=>
+            clawTrans.Find("left/downGanZi").DOLocalRotateQuaternion(Quaternion.Euler(0,0, 0),0.5f).SetDelay(1);
+            clawTrans.Find("right/downGanZi").DOLocalRotateQuaternion(Quaternion.Euler(0, 0, 0), 0.5f).SetDelay(1).OnComplete(()=>
             {
                 clawTrans.DOLocalMoveY(startPos.y, 3).SetDelay(1).SetEase(Ease.Linear);
             });
